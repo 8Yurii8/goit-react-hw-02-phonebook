192bb8de99a5ac74877bb60a9d4197ff63b5e16d
8Yurii8 <114691896+8Yurii8@users.noreply.github.com>
2023-05-11 20:50:52 +0300

    corr

--- src/components/Phonebook/ContactForm.jsx
+++ src/components/Phonebook/ContactForm.jsx
@@ -15,13 +15,13 @@ function ContactForm(props) {
             /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/,
             "Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           )
-          .required('Обов’язково'),
+          .required('required'),
         number: Yup.string()
           .matches(
             /^[+]?[0-9]{1,4}?[-.\s]?[(]?[0-9]{1,3}[)]?[-.\s]?[0-9]{1,4}[-.\s]?[0-9]{1,4}[-.\s]?[0-9]{1,9}$/,
             'Phone number must be digits and can contain spaces, dashes, parentheses and can start with +'
           )
-          .required('Обов’язково'),
+          .required('required'),
       })}
       onSubmit={handleAddContact}
     >
